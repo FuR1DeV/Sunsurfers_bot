0628a26980d5f686bbbb61290efa61cf1bc015b6
Vasia <vasia19rus@bk.ru>
2022-09-30 21:46:17 +0600

    first

---
+++ bot.py
@@ -0,0 +1,6 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from settings import config
+
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot, storage=MemoryStorage())
